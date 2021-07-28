5d2e1bbdee2d29d9b3a593ef4a8700d8f7e2ce09
joseguilherme96 <jose.guilherme96@outlook.com.br>
2021-07-28 20:34:38 -0300

    Revert "Incluso novas mensagens"
    This reverts commit 2a8601edf28afb71d9ccc577a302668ee97215bd.

--- php/mensagens.php
+++ php/mensagens.php
@@ -6,11 +6,6 @@ function mensagem($codmsg){
                         1=>'Usuário não foi localizado em nosso banco de dados',
                         2=>'Falha ao conectar com o banco de dados',
                         3=>'Conectado com sucesso',
-                        4=>'O link de redefinição de senha foi enviado para seu email',
-                        5=>'Usuario não localizado',
-                        6=>'Sua senha foi redefinida com sucesso',
-                        7=>'Seu usuário ainda não foi confirmado,por favor acesse seu email.',
-                        8=>'Usuário confirmado com sucesso',
                     );
 
     return $mensagem[$codmsg];
