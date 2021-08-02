4b126d77eb2d242eed0c781515b2ddb1459c998d
joseguilherme96 <jose.guilherme96@outlook.com.br>
2021-08-01 23:12:24 -0300

    alterado caminhos dos arquivos

--- php/conexao-banco.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $host = "LOCALHOST"; //Servidor
-    $usuario = "root"; //Usuário do Servidor
-    $senha = ""; //Senha do servidor
-    $banco=""; //Nome do banco
-
-    $conecta = new mysqli($host,$usuario,$senha,$banco);
-
-?>
