f93e463bd33778458b82c8e8f23b3931a2cbec72
Aeviles Aguiar Silva <52088444+aevilesaguiar@users.noreply.github.com>
2021-08-02 18:25:00 -0300

    commit banco de dados
    banco de dados alterado
    
    Co-Authored-By: claudiofsilvasantos <87715764+claudiofsilvasantos@users.noreply.github.com>

--- sql/localhost.sql
+++ sql/localhost.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: 30-Jul-2021 às 17:12
+-- Generation Time: 02-Ago-2021 às 16:25
 -- Versão do servidor: 5.6.34
 -- PHP Version: 7.1.11
 
@@ -27,32 +27,86 @@ USE `doe_um_sorriso`;
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `aprovar_cadastro_pf`
+--
+
+CREATE TABLE `aprovar_cadastro_pf` (
+  `id_aprovar_cadastro` int(11) NOT NULL,
+  `id_pf_aprovar_cadastro` int(11) DEFAULT NULL,
+  `status_aprovar_cadastro` char(1) DEFAULT NULL,
+  `id_colaborador_cadastro_pf` int(11) DEFAULT NULL,
+  `observacao_aprovar_cadastro_pf` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `aprovar_cadastro_pj`
+--
+
+CREATE TABLE `aprovar_cadastro_pj` (
+  `id_aprovar_cadastro_pj` int(11) NOT NULL,
+  `id_pj_aprovar_cadastro` int(11) DEFAULT NULL,
+  `status_aprovar_cadastro` char(1) DEFAULT NULL,
+  `id_colaborador_cadastro_pj` int(11) DEFAULT NULL,
+  `observacao_aprovar_cadastro_pj` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `cadastro_colaborador`
+--
+
+CREATE TABLE `cadastro_colaborador` (
+  `id_colaborador` int(11) NOT NULL,
+  `cpf_colaborador` varchar(14) NOT NULL,
+  `nome_colaborador` varchar(30) DEFAULT NULL,
+  `email_colaborador` varchar(50) DEFAULT NULL,
+  `telefone_colaborador` varchar(15) DEFAULT NULL,
+  `cep_colaborador` varchar(9) DEFAULT NULL,
+  `endereco_colaborador` varchar(50) DEFAULT NULL,
+  `numero_colaborador` int(5) DEFAULT NULL,
+  `cidade_colaborador` varchar(30) DEFAULT NULL,
+  `estado_colaborador` int(2) DEFAULT NULL,
+  `bairro_colaborador` varchar(30) DEFAULT NULL,
+  `complemento_colaborador` varchar(10) DEFAULT NULL,
+  `funcao_colaborador` varchar(15) DEFAULT NULL,
+  `nome_organizacao_colaborador` int(11) DEFAULT NULL,
+  `usuario_colaborador` int(11) DEFAULT NULL,
+  `descricao_de_cargo_colaborador` varchar(30) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `cadastro_familia`
 --
 
 CREATE TABLE `cadastro_familia` (
   `id_familia` int(11) NOT NULL,
-  `cpf_mae` int(11) NOT NULL,
+  `cpf_mae` varchar(14) NOT NULL,
   `nome_mae` varchar(30) NOT NULL,
-  `cpf_pai` int(11) DEFAULT NULL,
+  `cpf_pai` varchar(14) DEFAULT NULL,
+  `rg_crianca` varchar(12) NOT NULL,
   `nome_crianca` varchar(30) NOT NULL,
-  `rg_crianca` int(9) NOT NULL,
   `data_de_aniversario` date DEFAULT NULL,
   `sexo_crianca` char(1) DEFAULT NULL,
   `e_mail_familia` varchar(30) DEFAULT NULL,
-  `telefone_familia` int(11) NOT NULL,
+  `telefone_familia` varchar(15) NOT NULL,
+  `cep_familia` varchar(9) DEFAULT NULL,
   `endereco_familia` varchar(50) DEFAULT NULL,
-  `cep_familia` int(8) DEFAULT NULL,
   `numero_familia` int(5) DEFAULT NULL,
   `cidade_familia` varchar(30) DEFAULT NULL,
-  `estado_familia` int(2) DEFAULT NULL,
+  `estado_familia` varchar(2) NOT NULL,
   `bairro_familia` varchar(15) DEFAULT NULL,
   `complemento_familia` varchar(10) DEFAULT NULL,
-  `tamanho_calca_familia` int(11) DEFAULT NULL,
-  `tamanho_camiseta_familia` int(11) DEFAULT NULL,
-  `tamanho_calcado_familia` int(11) DEFAULT NULL,
+  `tamanho_calca_familia` int(11) NOT NULL,
+  `tamanho_camiseta_familia` int(11) NOT NULL,
+  `tamanho_calcado_familia` int(11) NOT NULL,
+  `sugestao_presente` varchar(45) DEFAULT NULL,
   `observacao_familia` varchar(40) DEFAULT NULL,
-  `org_familia` int(11) DEFAULT NULL
+  `org_familia` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -63,19 +117,21 @@ CREATE TABLE `cadastro_familia` (
 
 CREATE TABLE `cadastro_org` (
   `id_org` int(11) NOT NULL,
-  `cnpj_org` int(12) NOT NULL,
+  `cnpj_org` varchar(17) NOT NULL,
   `razao_social_org` varchar(30) NOT NULL,
   `email_org` varchar(50) DEFAULT NULL,
-  `telefone_org` int(11) DEFAULT NULL,
-  `cep_org` int(8) DEFAULT NULL,
+  `telefone_org` varchar(15) DEFAULT NULL,
+  `cep_org` varchar(9) DEFAULT NULL,
   `endereço_org` varchar(50) DEFAULT NULL,
   `numero_org` int(5) DEFAULT NULL,
   `cidade_org` varchar(30) DEFAULT NULL,
   `estado_org` int(2) DEFAULT NULL,
   `bairro_org` varchar(15) DEFAULT NULL,
   `complemento_org` varchar(10) DEFAULT NULL,
+  `site_org` varchar(45) DEFAULT NULL,
   `redesocial_org` varchar(40) DEFAULT NULL,
   `tipo_de_org` varchar(40) DEFAULT NULL,
+  `usuario_org` int(11) DEFAULT NULL,
   `obs_org` varchar(40) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -87,21 +143,21 @@ CREATE TABLE `cadastro_org` (
 
 CREATE TABLE `cadastro_pf` (
   `id_pf` int(11) NOT NULL,
-  `cpf_pf` int(11) NOT NULL,
+  `cpf_pf` varchar(14) NOT NULL,
   `nome_pf` varchar(30) DEFAULT NULL,
   `email_pf` varchar(50) DEFAULT NULL,
-  `telefone_pf` int(11) DEFAULT NULL,
-  `cep_pf` int(8) DEFAULT NULL,
+  `telefone_pf` varchar(15) DEFAULT NULL,
+  `cep_pf` varchar(9) DEFAULT NULL,
   `endereço_pf` varchar(50) DEFAULT NULL,
   `numero_pf` int(5) DEFAULT NULL,
+  `cidade_pf` varchar(45) DEFAULT NULL,
   `estado_pf` int(2) DEFAULT NULL,
   `bairro_pf` varchar(15) DEFAULT NULL,
   `complemento_pf` varchar(10) DEFAULT NULL,
-  `redesocial_pf` varchar(40) DEFAULT NULL,
-  `selecionar_crianca_pf` int(11) DEFAULT NULL,
   `selecione_ong_pf` int(11) DEFAULT NULL,
-  `usuario_pf` varchar(30) DEFAULT NULL,
-  `senha_pf` varchar(15) DEFAULT NULL
+  `selecionar_crianca_pf` int(11) DEFAULT NULL,
+  `redesocial_pf` varchar(40) DEFAULT NULL,
+  `usuario_pf` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -112,11 +168,11 @@ CREATE TABLE `cadastro_pf` (
 
 CREATE TABLE `cadastro_pj` (
   `id_pj` int(11) NOT NULL,
-  `cnpj_pj` int(12) NOT NULL,
+  `cnpj_pj` varchar(17) NOT NULL,
   `razao_social_pj` varchar(30) DEFAULT NULL,
   `email_pj` varchar(50) DEFAULT NULL,
-  `telefone_pj` int(11) DEFAULT NULL,
-  `cep_pj` int(8) DEFAULT NULL,
+  `telefone_pj` varchar(15) DEFAULT NULL,
+  `cep_pj` varchar(9) DEFAULT NULL,
   `endereço_pj` varchar(50) DEFAULT NULL,
   `numero_pj` int(5) DEFAULT NULL,
   `cidade_pj` varchar(30) DEFAULT NULL,
@@ -127,22 +183,7 @@ CREATE TABLE `cadastro_pj` (
   `redesocial_pj` varchar(40) DEFAULT NULL,
   `selecione_ong_pj` int(11) DEFAULT NULL,
   `selecionar_crianca_pj` int(11) DEFAULT NULL,
-  `usuario_pj` varchar(30) DEFAULT NULL,
-  `senha_pj` varchar(15) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `confirmacao_da_entrega`
---
-
-CREATE TABLE `confirmacao_da_entrega` (
-  `id_confirmacao` int(11) NOT NULL,
-  `nome_doador_entrega` int(11) NOT NULL,
-  `dados_familia_entrega` int(11) DEFAULT NULL,
-  `status_entrega` char(1) DEFAULT NULL,
-  `data_hora` datetime DEFAULT NULL
+  `usuario_pj` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -153,10 +194,21 @@ CREATE TABLE `confirmacao_da_entrega` (
 
 CREATE TABLE `dados_doacao` (
   `id_doacao` int(11) NOT NULL,
-  `organizacao_doacao` int(11) DEFAULT NULL,
+  `organizacao_doacao1` int(11) DEFAULT NULL,
   `nome_doacao` int(11) DEFAULT NULL,
-  `tipo_kit_doacao` int(11) DEFAULT NULL,
-  `aprovacao_doacao` char(1) DEFAULT NULL
+  `tipo_kit` int(11) DEFAULT NULL,
+  `status_kit` char(1) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `deixe_seu_email`
+--
+
+CREATE TABLE `deixe_seu_email` (
+  `id_email` int(11) NOT NULL,
+  `e_mail` varchar(30) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -193,6 +245,21 @@ CREATE TABLE `fale_conosco` (
   `email` varchar(50) DEFAULT NULL,
   `telefone` int(11) DEFAULT NULL,
   `mensagem` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `lista_confirmacao_entrega`
+--
+
+CREATE TABLE `lista_confirmacao_entrega` (
+  `id_confirmacao` int(11) NOT NULL,
+  `nome_doador_entrega` int(11) NOT NULL,
+  `dados_familia_entrega` int(11) DEFAULT NULL,
+  `status_entrega` char(1) DEFAULT NULL,
+  `data_hora` datetime DEFAULT NULL,
+  `dados_doacao_reprovado` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -271,6 +338,30 @@ CREATE TABLE `usuarios` (
 --
 
 --
+-- Indexes for table `aprovar_cadastro_pf`
+--
+ALTER TABLE `aprovar_cadastro_pf`
+  ADD PRIMARY KEY (`id_aprovar_cadastro`),
+  ADD KEY `id_pf_aprovar_cadastro` (`id_pf_aprovar_cadastro`),
+  ADD KEY `id_colaborador_cadastro_pf` (`id_colaborador_cadastro_pf`);
+
+--
+-- Indexes for table `aprovar_cadastro_pj`
+--
+ALTER TABLE `aprovar_cadastro_pj`
+  ADD PRIMARY KEY (`id_aprovar_cadastro_pj`),
+  ADD KEY `id_pj_aprovar_cadastro` (`id_pj_aprovar_cadastro`),
+  ADD KEY `id_colaborador_cadastro_pj` (`id_colaborador_cadastro_pj`);
+
+--
+-- Indexes for table `cadastro_colaborador`
+--
+ALTER TABLE `cadastro_colaborador`
+  ADD PRIMARY KEY (`id_colaborador`),
+  ADD KEY `estado_colaborador` (`estado_colaborador`),
+  ADD KEY `usuario_colaborador` (`usuario_colaborador`);
+
+--
 -- Indexes for table `cadastro_familia`
 --
 ALTER TABLE `cadastro_familia`
@@ -286,7 +377,8 @@ ALTER TABLE `cadastro_familia`
 --
 ALTER TABLE `cadastro_org`
   ADD PRIMARY KEY (`id_org`),
-  ADD KEY `estado_org` (`estado_org`);
+  ADD KEY `estado_org` (`estado_org`),
+  ADD KEY `usuario_org` (`usuario_org`);
 
 --
 -- Indexes for table `cadastro_pf`
@@ -295,7 +387,8 @@ ALTER TABLE `cadastro_pf`
   ADD PRIMARY KEY (`id_pf`),
   ADD KEY `estado_pf` (`estado_pf`),
   ADD KEY `selecione_ong_pf` (`selecione_ong_pf`),
-  ADD KEY `selecionar_crianca_pf` (`selecionar_crianca_pf`);
+  ADD KEY `selecionar_crianca_pf` (`selecionar_crianca_pf`),
+  ADD KEY `usuario_pf` (`usuario_pf`);
 
 --
 -- Indexes for table `cadastro_pj`
@@ -304,24 +397,23 @@ ALTER TABLE `cadastro_pj`
   ADD PRIMARY KEY (`id_pj`),
   ADD KEY `estado_pj` (`estado_pj`),
   ADD KEY `selecione_ong_pj` (`selecione_ong_pj`),
-  ADD KEY `selecionar_crianca_pj` (`selecionar_crianca_pj`);
-
---
--- Indexes for table `confirmacao_da_entrega`
---
-ALTER TABLE `confirmacao_da_entrega`
-  ADD PRIMARY KEY (`id_confirmacao`),
-  ADD KEY `dados_familia_entrega` (`dados_familia_entrega`),
-  ADD KEY `nome_doador_entrega` (`nome_doador_entrega`);
+  ADD KEY `selecionar_crianca_pj` (`selecionar_crianca_pj`),
+  ADD KEY `usuario_pj` (`usuario_pj`);
 
 --
 -- Indexes for table `dados_doacao`
 --
 ALTER TABLE `dados_doacao`
   ADD PRIMARY KEY (`id_doacao`),
-  ADD KEY `tipo_kit_doacao` (`tipo_kit_doacao`),
-  ADD KEY `organizacao_doacao` (`organizacao_doacao`),
-  ADD KEY `nome_doacao` (`nome_doacao`);
+  ADD KEY `nome_doacao` (`nome_doacao`),
+  ADD KEY `tipo_kit` (`tipo_kit`),
+  ADD KEY `organizacao_doacao1` (`organizacao_doacao1`);
+
+--
+-- Indexes for table `deixe_seu_email`
+--
+ALTER TABLE `deixe_seu_email`
+  ADD PRIMARY KEY (`id_email`);
 
 --
 -- Indexes for table `esqueceu_senha`
@@ -340,6 +432,14 @@ ALTER TABLE `estados`
 --
 ALTER TABLE `fale_conosco`
   ADD PRIMARY KEY (`id_mensagem`);
+
+--
+-- Indexes for table `lista_confirmacao_entrega`
+--
+ALTER TABLE `lista_confirmacao_entrega`
+  ADD PRIMARY KEY (`id_confirmacao`),
+  ADD KEY `dados_familia_entrega` (`dados_familia_entrega`),
+  ADD KEY `nome_doador_entrega` (`nome_doador_entrega`);
 
 --
 -- Indexes for table `lista_de_recebimento_doações`
@@ -437,20 +537,32 @@ ALTER TABLE `usuarios`
 --
 
 --
--- Limitadores para a tabela `cadastro_familia`
+-- Limitadores para a tabela `aprovar_cadastro_pf`
 --
-ALTER TABLE `cadastro_familia`
-  ADD CONSTRAINT `estado_familia` FOREIGN KEY (`estado_familia`) REFERENCES `estados` (`id_estado`),
-  ADD CONSTRAINT `org_familia` FOREIGN KEY (`org_familia`) REFERENCES `cadastro_org` (`id_org`),
-  ADD CONSTRAINT `tamanho_calca_familia` FOREIGN KEY (`tamanho_calca_familia`) REFERENCES `tamanho_calca` (`id_calca`),
-  ADD CONSTRAINT `tamanho_calcado_familia` FOREIGN KEY (`tamanho_calcado_familia`) REFERENCES `tamanho_calcado` (`id_calcado`),
-  ADD CONSTRAINT `tamanho_camiseta_familia` FOREIGN KEY (`tamanho_camiseta_familia`) REFERENCES `tamanho_camiseta` (`id_camiseta`);
+ALTER TABLE `aprovar_cadastro_pf`
+  ADD CONSTRAINT `id_colaborador_cadastro_pf` FOREIGN KEY (`id_colaborador_cadastro_pf`) REFERENCES `cadastro_colaborador` (`id_colaborador`),
+  ADD CONSTRAINT `id_pf_aprovar_cadastro` FOREIGN KEY (`id_pf_aprovar_cadastro`) REFERENCES `cadastro_pf` (`id_pf`);
+
+--
+-- Limitadores para a tabela `aprovar_cadastro_pj`
+--
+ALTER TABLE `aprovar_cadastro_pj`
+  ADD CONSTRAINT `id_colaborador_cadastro_pj` FOREIGN KEY (`id_colaborador_cadastro_pj`) REFERENCES `cadastro_colaborador` (`id_colaborador`),
+  ADD CONSTRAINT `id_pj_aprovar_cadastro` FOREIGN KEY (`id_pj_aprovar_cadastro`) REFERENCES `cadastro_pj` (`id_pj`);
+
+--
+-- Limitadores para a tabela `cadastro_colaborador`
+--
+ALTER TABLE `cadastro_colaborador`
+  ADD CONSTRAINT `estado_colaborador` FOREIGN KEY (`estado_colaborador`) REFERENCES `estados` (`id_estado`),
+  ADD CONSTRAINT `usuario_colaborador` FOREIGN KEY (`usuario_colaborador`) REFERENCES `usuarios` (`id_usuario`);
 
 --
 -- Limitadores para a tabela `cadastro_org`
 --
 ALTER TABLE `cadastro_org`
-  ADD CONSTRAINT `cadastro_org_ibfk_1` FOREIGN KEY (`estado_org`) REFERENCES `estados` (`id_estado`);
+  ADD CONSTRAINT `cadastro_org_ibfk_1` FOREIGN KEY (`estado_org`) REFERENCES `estados` (`id_estado`),
+  ADD CONSTRAINT `usuario_org` FOREIGN KEY (`usuario_org`) REFERENCES `usuarios` (`id_usuario`);
 
 --
 -- Limitadores para a tabela `cadastro_pf`
@@ -458,7 +570,8 @@ ALTER TABLE `cadastro_org`
 ALTER TABLE `cadastro_pf`
   ADD CONSTRAINT `cadastro_pf_ibfk_1` FOREIGN KEY (`estado_pf`) REFERENCES `estados` (`id_estado`),
   ADD CONSTRAINT `cadastro_pf_ibfk_2` FOREIGN KEY (`selecione_ong_pf`) REFERENCES `cadastro_org` (`id_org`),
-  ADD CONSTRAINT `selecionar_crianca_pf` FOREIGN KEY (`selecionar_crianca_pf`) REFERENCES `cadastro_familia` (`id_familia`);
+  ADD CONSTRAINT `selecionar_crianca_pf` FOREIGN KEY (`selecionar_crianca_pf`) REFERENCES `cadastro_familia` (`id_familia`),
+  ADD CONSTRAINT `usuario_pf` FOREIGN KEY (`usuario_pf`) REFERENCES `usuarios` (`id_usuario`);
 
 --
 -- Limitadores para a tabela `cadastro_pj`
@@ -466,20 +579,23 @@ ALTER TABLE `cadastro_pf`
 ALTER TABLE `cadastro_pj`
   ADD CONSTRAINT `cadastro_pj_ibfk_1` FOREIGN KEY (`estado_pj`) REFERENCES `estados` (`id_estado`),
   ADD CONSTRAINT `cadastro_pj_ibfk_2` FOREIGN KEY (`selecione_ong_pj`) REFERENCES `cadastro_org` (`id_org`),
-  ADD CONSTRAINT `selecionar_crianca_pj` FOREIGN KEY (`selecionar_crianca_pj`) REFERENCES `cadastro_familia` (`id_familia`);
-
---
--- Limitadores para a tabela `confirmacao_da_entrega`
---
-ALTER TABLE `confirmacao_da_entrega`
-  ADD CONSTRAINT `dados_familia_entrega` FOREIGN KEY (`dados_familia_entrega`) REFERENCES `cadastro_familia` (`id_familia`),
-  ADD CONSTRAINT `nome_doador_entrega` FOREIGN KEY (`nome_doador_entrega`) REFERENCES `usuarios` (`id_usuario`);
+  ADD CONSTRAINT `selecionar_crianca_pj` FOREIGN KEY (`selecionar_crianca_pj`) REFERENCES `cadastro_familia` (`id_familia`),
+  ADD CONSTRAINT `usuario_pj` FOREIGN KEY (`usuario_pj`) REFERENCES `usuarios` (`id_usuario`);
 
 --
 -- Limitadores para a tabela `dados_doacao`
 --
 ALTER TABLE `dados_doacao`
-  ADD CONSTRAINT `nome_doacao` FOREIGN KEY (`nome_doacao`) REFERENCES `cadastro_familia` (`id_familia`);
+  ADD CONSTRAINT `nome_doacao` FOREIGN KEY (`nome_doacao`) REFERENCES `cadastro_familia` (`id_familia`),
+  ADD CONSTRAINT `organizacao_doacao1` FOREIGN KEY (`organizacao_doacao1`) REFERENCES `cadastro_org` (`id_org`),
+  ADD CONSTRAINT `tipo_kit` FOREIGN KEY (`tipo_kit`) REFERENCES `tipo_de_kit` (`id_kit`);
+
+--
+-- Limitadores para a tabela `lista_confirmacao_entrega`
+--
+ALTER TABLE `lista_confirmacao_entrega`
+  ADD CONSTRAINT `dados_familia_entrega` FOREIGN KEY (`dados_familia_entrega`) REFERENCES `cadastro_familia` (`id_familia`),
+  ADD CONSTRAINT `nome_doador_entrega` FOREIGN KEY (`nome_doador_entrega`) REFERENCES `usuarios` (`id_usuario`);
 
 --
 -- Limitadores para a tabela `lista_de_recebimento_doações`
@@ -487,7 +603,7 @@ ALTER TABLE `dados_doacao`
 ALTER TABLE `lista_de_recebimento_doações`
   ADD CONSTRAINT `doador` FOREIGN KEY (`doador`) REFERENCES `usuarios` (`id_usuario`),
   ADD CONSTRAINT `nome_crianca` FOREIGN KEY (`nome_crianca`) REFERENCES `cadastro_familia` (`id_familia`),
-  ADD CONSTRAINT `status_de_recebimento` FOREIGN KEY (`status_de_recebimento`) REFERENCES `confirmacao_da_entrega` (`id_confirmacao`);
+  ADD CONSTRAINT `status_de_recebimento` FOREIGN KEY (`status_de_recebimento`) REFERENCES `lista_confirmacao_entrega` (`id_confirmacao`);
 --
 -- Database: `pwii`
 --
