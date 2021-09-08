e72d88617ac2a6c421bf0a5b1ec25f4810ac4e6d
joseguilherme96 <jose.guilherme96@outlook.com.br>
2021-09-07 22:03:33 -0300

    Novas funções de sessão
    Função inclui_dados_criança salva dados da criança  na pagina para incluir posteriormente no banco de dados.
    
    Função limpa_dados_criança limpa dados caso usuário escolha outra organização, assim desvincula com a organização escolhida anteriormente.

--- php/controle-site/sessao.php
+++ php/controle-site/sessao.php
@@ -50,16 +50,37 @@ function inlui_organizacao($id_cadastro,$nome_organizacao,$cidade,$estado,$acao)
 }
 
 function inlui_dados_criança($rg_criança,$nome_criança,$idade,$sexo,
-$tipo_kit,$tamanho_calca,$tamanho_camisa,$tamanho_calcado){
+$tipo_kit,$tamanho_calca,$tamanho_camisa,$tamanho_calcado,$acao){
 
     $_SESSION['doacao']['rg_crianca'] = $rg_criança;
-    $_SESSION['doacao']['nome_crianca'] = $rg_criança;
+    $_SESSION['doacao']['nome_crianca'] = $nome_criança;
     $_SESSION['doacao']['idade'] = $idade;
     $_SESSION['doacao']['sexo'] = $sexo;
     $_SESSION['doacao']['tipo_kit'] = $tipo_kit;
-    $_SESSION['doacao']['tamanho_calca'] = $tipo_kit;
-    $_SESSION['doacao']['tamanho_camisa'] = $tipo_kit;
-    $_SESSION['doacao']['tipo_calcado'] = $tipo_kit;
+    $_SESSION['doacao']['tamanho_calca'] = $tamanho_calca;
+    $_SESSION['doacao']['tamanho_camisa'] = $tamanho_camisa;
+    $_SESSION['doacao']['tipo_calcado'] = $tamanho_calcado;
+    $_SESSION['doacao']['acaocriancakit']=$acao;
+    $_SESSION['doacao']['crianca_selecionada']=$_SESSION['doacao']['rg_crianca']."/".$_SESSION['doacao']['nome_crianca'].
+    " - ".$_SESSION['doacao']['idade']."/".$_SESSION['doacao']['sexo']."/".$_SESSION['doacao']['tipo_kit']
+    ."/".$_SESSION['doacao']['tamanho_calca']."/".$_SESSION['doacao']['tamanho_camisa']."/".$_SESSION['doacao']['tipo_calcado'];
+    $_SESSION['doacao']['crianca_selecionada_exib']=$_SESSION['doacao']['nome_crianca'].
+    "-".$_SESSION['doacao']['idade']."-".$_SESSION['doacao']['sexo']
+    ;
+}
 
+function limpa_dados_criança(){
+
+    unset($_SESSION['doacao']['rg_crianca']);
+    unset($_SESSION['doacao']['nome_crianca']);
+    unset($_SESSION['doacao']['idade']);
+    unset($_SESSION['doacao']['sexo']);
+    unset($_SESSION['doacao']['tipo_kit']);
+    unset($_SESSION['doacao']['tamanho_calca']);
+    unset($_SESSION['doacao']['tamanho_camisa']);
+    unset($_SESSION['doacao']['tipo_calcado']);
+    unset($_SESSION['doacao']['acaocriancakit']);
+    unset($_SESSION['doacao']['crianca_selecionada']);
+    unset($_SESSION['doacao']['crianca_selecionada_exib']);
 }
 ?>
