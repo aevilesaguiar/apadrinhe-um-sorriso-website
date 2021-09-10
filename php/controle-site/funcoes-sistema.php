9fa032b2dacee484c7b9210fe7c7f99cc557ba72
joseguilherme96 <jose.guilherme96@outlook.com.br>
2021-09-09 22:02:38 -0300

    Nova função de sistema
    Função exibi dados do usuário na sessão

--- php/controle-site/funcoes-sistema.php
+++ php/controle-site/funcoes-sistema.php
@@ -5,10 +5,21 @@ function exibe_doacao($nome){
     return isset($_SESSION['doacao'][$nome])?$_SESSION['doacao'][$nome]:"";
 }
 
+function exibe_doador($nome){
+    
+    return isset($_SESSION['usuario'][$nome])?$_SESSION['usuario'][$nome]:"";
+}
+
+function data_hora(){
+    date_default_timezone_set('America/Sao_Paulo');
+
+    return date("Y-m-d H:m:s");
+}
+
 function calcula_idade($data_nascimento){
 
     if($data_nascimento!==""){
-    date_default_timezone_set('America/Sao_Paulo');
+    data_hora();
     $data_atual		=	explode("-",date("Y-m-d"));
     $data_nascimento=explode("-",$data_nascimento);
 
