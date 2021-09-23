2b511df565c27df3a387b7fb87a54f865b714fc3
joseguilherme96 <jose.guilherme96@outlook.com.br>
2021-09-23 18:19:05 -0300

    Funções de Alterações
    * Responsável por atualizar os dados dos usuários

---
+++ php/controle-site/funcoes-alteracao.php
@@ -0,0 +1,41 @@
+<?php
+
+function altera_dados_gerais($nome,$telefone,$rede_social,
+$numendereco,$logradouro,$cidade,$estado,$cep,$bairro,
+$complemento,$id_cadastro){
+
+    
+    $cadastro = 'UPDATE perfil SET
+        nome="'.$nome.'",telefone="'.$telefone.'",
+        rede_social="'.$rede_social.'",numendereco="'.$numendereco.'",logradouro="'.$logradouro.'",cidade="'.$cidade.'",
+        estado="'.$estado.'",cep="'.$cep.'",
+        bairro="'.$bairro.'",complemento="'.$complemento.'" WHERE id_cadastro="'.$id_cadastro.'"';
+
+    return $cadastro;
+    
+}
+
+//Cadastra pj
+function altera_pj($cnpj,$nome_fantasia,$site,$tipo_pj,$fk_id_cadastro){
+    $cadastro = 'INSERT INTO dados_pj(
+        cnpj,nome_fantasia,site,tipo_pj,
+    fk_id_cadastro) 
+    VALUES("'.$cnpj.'","'.$nome_fantasia.'","'.$site.'","'.$tipo_pj.'","'.$fk_id_cadastro.'")';
+
+    return $cadastro;
+    
+}
+
+//Cadastra pf
+function altera_pf($cpf,$dados_gerais_id){
+    $cadastro = 'INSERT INTO dados_pf(cpf,rg,fk_id_cadastro) 
+    VALUES("'.$cpf.'","0000000000","'.$dados_gerais_id.'")';
+
+    return $cadastro;
+    
+}
+
+function altera_usuario($usuario,$senha){
+    $cadastro = 'UPDATE usuario SET senha="'.$senha.'" WHERE user="'.$usuario.'"';
+    return $cadastro;
+}
