0f64cd3332f3afe40271c06b5391b59c3b29fa41
joseguilherme96 <jose.guilherme96@outlook.com.br>
2021-09-24 20:27:35 -0300

    Banco com dados
    * Apenas para testes com dados já cadastrados.

--- sql/doe_um_sorriso.sql
+++ sql/doe_um_sorriso.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 28-Ago-2021 às 23:34
+-- Tempo de geração: 24-Set-2021 às 23:23
 -- Versão do servidor: 5.7.31
 -- versão do PHP: 7.1.33
 
@@ -66,7 +66,8 @@ CREATE TABLE IF NOT EXISTS `cadastra` (
 
 INSERT INTO `cadastra` (`fk_rg_crianca`, `fk_id_cadastro`) VALUES
 ('58.656.656-6', 41),
-('66.658.695-9', 43);
+('66.658.695-9', 43),
+('58.695.695-6', 43);
 
 -- --------------------------------------------------------
 
@@ -108,6 +109,7 @@ CREATE TABLE IF NOT EXISTS `dados_crianca` (
 
 INSERT INTO `dados_crianca` (`rg_crianca`, `nome_crianca`, `sexo`, `nasc_crianca`, `tamanho_camiseta`, `tamanho_sapato`, `tamanho_calca`) VALUES
 ('58.656.656-6', 'Isaque', 'M', '2000-10-21', '16', '20.5', '8'),
+('58.695.695-6', 'Jeison', 'M', '2000-10-31', '8', '16', '6'),
 ('66.658.695-9', 'Maria Francisca', 'F', '2001-06-23', '4', '14', '10');
 
 -- --------------------------------------------------------
@@ -131,6 +133,9 @@ CREATE TABLE IF NOT EXISTS `dados_pf` (
 
 INSERT INTO `dados_pf` (`cpf`, `rg`, `fk_id_cadastro`) VALUES
 ('256.625.652-69', '00000000', 43),
+('256.695.659-96', '0000000', 45),
+('458.515.454-54', '0000000000', 46),
+('545.555.454-54', '0000000000', 48),
 ('555.454.454-54', '0000000000', 40),
 ('565.695.669-56', '0000000', 42);
 
@@ -175,7 +180,7 @@ CREATE TABLE IF NOT EXISTS `dados_responsavel` (
   PRIMARY KEY (`id_familia`),
   KEY `fk_id_cadastro` (`fk_id_cadastro`),
   KEY `fk_cpf` (`fk_cpf`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `dados_responsavel`
@@ -183,7 +188,8 @@ CREATE TABLE IF NOT EXISTS `dados_responsavel` (
 
 INSERT INTO `dados_responsavel` (`id_familia`, `cpf_resp`, `nome_resp`, `fk_cpf`, `fk_id_cadastro`) VALUES
 (1, '----------', '-----------', '565.695.669-56', 42),
-(2, '----------', '-----------', '565.695.669-56', 43);
+(2, '----------', '-----------', '565.695.669-56', 43),
+(3, NULL, NULL, '256.695.659-96', 45);
 
 -- --------------------------------------------------------
 
@@ -194,7 +200,7 @@ INSERT INTO `dados_responsavel` (`id_familia`, `cpf_resp`, `nome_resp`, `fk_cpf`
 DROP TABLE IF EXISTS `doacao`;
 CREATE TABLE IF NOT EXISTS `doacao` (
   `id_doacao` int(11) NOT NULL AUTO_INCREMENT,
-  `status_doacao` enum('S','N') DEFAULT NULL,
+  `status_doacao` varchar(10) DEFAULT NULL,
   `data_hora_entrega` datetime DEFAULT NULL,
   `data_hora_selecao` datetime DEFAULT NULL,
   `data_hora_recebimento` datetime DEFAULT NULL,
@@ -202,7 +208,17 @@ CREATE TABLE IF NOT EXISTS `doacao` (
   `fk_rg_crianca` varchar(12) DEFAULT NULL,
   PRIMARY KEY (`id_doacao`),
   KEY `fk_rg_crianca` (`fk_rg_crianca`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=56 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `doacao`
+--
+
+INSERT INTO `doacao` (`id_doacao`, `status_doacao`, `data_hora_entrega`, `data_hora_selecao`, `data_hora_recebimento`, `tipo_presente`, `fk_rg_crianca`) VALUES
+(52, 'FINALIZADO', '2021-09-20 12:09:58', '2021-09-21 12:09:58', '2021-09-21 12:09:58', 'KIT COMPLETO', '66.658.695-9'),
+(53, 'PENDENTE', '2021-09-20 12:09:13', NULL, NULL, 'KIT SIMPLES', '58.656.656-6'),
+(54, 'PENDENTE', NULL, '2021-09-23 14:09:58', NULL, 'KIT COMPLETO', '66.658.695-9'),
+(55, 'PENDENTE', NULL, '2021-09-23 18:09:53', NULL, 'KIT SIMPLES', '66.658.695-9');
 
 -- --------------------------------------------------------
 
@@ -218,6 +234,14 @@ CREATE TABLE IF NOT EXISTS `doa_exibe` (
   KEY `fk_id_mensagem` (`fk_id_mensagem`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `doa_exibe`
+--
+
+INSERT INTO `doa_exibe` (`fk_id_doacao`, `fk_id_mensagem`) VALUES
+(53, 6),
+(54, 7);
+
 -- --------------------------------------------------------
 
 --
@@ -228,10 +252,21 @@ DROP TABLE IF EXISTS `fale_conosco`;
 CREATE TABLE IF NOT EXISTS `fale_conosco` (
   `e_mail_fale_conosco` varchar(45) NOT NULL,
   `nome` varchar(30) DEFAULT NULL,
-  `telefone` varchar(14) DEFAULT NULL,
+  `telefone` varchar(15) DEFAULT NULL,
   `mensagem` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`e_mail_fale_conosco`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `fale_conosco`
+--
+
+INSERT INTO `fale_conosco` (`e_mail_fale_conosco`, `nome`, `telefone`, `mensagem`) VALUES
+('josegfgfgmk8@hotmail.com', 'Jose', '(85) 4548-88888', ''),
+('josemk8@hotmail.com', 'Jose', '(85) 4548-88888', ''),
+('josemk8dfd@hotmail.com', 'Jose', '(85) 4548-88888', ''),
+('josemksss8@hotmail.com', 'Jose', '(85) 4548-88888', ''),
+('jrtrtosemk8@hotmail.com', 'Jose', '(45) 4545-45454', '');
 
 -- --------------------------------------------------------
 
@@ -247,6 +282,16 @@ CREATE TABLE IF NOT EXISTS `gerencia` (
   KEY `fk_id_cadastro` (`fk_id_cadastro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Extraindo dados da tabela `gerencia`
+--
+
+INSERT INTO `gerencia` (`fk_id_doacao`, `fk_id_cadastro`) VALUES
+(52, 43),
+(53, 41),
+(54, 43),
+(55, 43);
+
 -- --------------------------------------------------------
 
 --
@@ -256,10 +301,19 @@ CREATE TABLE IF NOT EXISTS `gerencia` (
 DROP TABLE IF EXISTS `mensagem_sistema`;
 CREATE TABLE IF NOT EXISTS `mensagem_sistema` (
   `id_mensagem` int(11) NOT NULL AUTO_INCREMENT,
-  `status_sistema` enum('S','N') DEFAULT NULL,
+  `status_sistema` varchar(10) DEFAULT NULL,
   `mensagem` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id_mensagem`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `mensagem_sistema`
+--
+
+INSERT INTO `mensagem_sistema` (`id_mensagem`, `status_sistema`, `mensagem`) VALUES
+(5, 'PENDENTE', 'Consulta CPF Inexist'),
+(6, 'FINALIZADO', 'Doação Reprovada'),
+(7, 'FINALIZADO', 'Doação Incompleta');
 
 -- --------------------------------------------------------
 
@@ -299,18 +353,21 @@ CREATE TABLE IF NOT EXISTS `perfil` (
   `fk_user` varchar(30) DEFAULT NULL,
   PRIMARY KEY (`id_cadastro`),
   KEY `fk_user` (`fk_user`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=49 DEFAULT CHARSET=latin1;
 
 --
 -- Extraindo dados da tabela `perfil`
 --
 
 INSERT INTO `perfil` (`id_cadastro`, `tipo_cadastro`, `nivel_acesso`, `status_cadastro`, `nome`, `telefone`, `rede_social`, `e_mail`, `numendereco`, `logradouro`, `cidade`, `estado`, `cep`, `bairro`, `complemento`, `fk_user`) VALUES
-(40, 'doador_pf', 0, 'EA', 'Jose', '(45) 8888-87777', 'facebook.com', 'ose@gmail.com', '454', 'Rua alameda', 'Mairinque', 'AC', '77774-555', 'Jardim Vi.', 'Lado', 'ose@gmail.com'),
-(41, 'organizacao', 0, 'EA', 'Sorriso compartilhado com vocÃª Ltda', '(55) 5888-88888', 'sorriso.com', 'sorriso@sorriso.com.br', '454', 'Rua alameda', 'Mairinque', 'AC', '77774-555', 'Jardim Vi.', 'Lado', 'sorriso@sorriso.com.br'),
+(40, 'doador_pf', 0, 'RP', 'Jose', '(45) 8855-54444', 'facebook.com', 'ose@gmail.com', '454', 'Rua alameda', 'Mairinque', 'AC', '77775-555', 'Jardim Vitoria', 'Perto da Oficina de Motos', 'ose@gmail.com'),
+(41, 'organizacao', 0, 'EA', 'Sorriso compartilhado com vocÃª Ltda', '(55) 5888-88888', 'sorriso.com', 'sorriDso@sorriso.com.br', '454', 'Rua alameda', 'Mairinque', 'AC', '77774-555', 'Jardim Vi.', 'Lado', 'sorriso@sorriso.com.br'),
 (42, 'familia', 0, 'OK', 'Francisca da Silva Oliveira', '(25) 4556-6699', 'facebook.com/francisca58', 'francisca@gmail.com', '254', 'Av. São Paulo', 'São Paulo', 'AC', '12256-000', 'São Pedro', 'Ao lado Paço Municipal', NULL),
 (43, 'organizacao', 0, 'EA', 'DOE PARA VIDA', '(54) 5454-54545', 'facebook.com/doeparaumaideia', 'doeparavida@doe.com', '58', 'Rua Francisco', 'SÃ£o Paulo', 'AC', '54454-488', 'Jardim floripa', 'Mosce', 'doeparavida@doe.com'),
-(44, 'familia', 0, 'OK', 'Maria Aparecida', '(52) 5858-5656', 'facebook.com/mariasilva', 'mariasilva@hotmail.com', '458', 'Av. Fast Fool', 'Sorocaba', 'SP', '45869-695', 'Jardim tempio', 'Sobrado', NULL);
+(44, 'familia', 0, 'OK', 'Maria Aparecida', '(52) 5858-5656', 'facebook.com/mariasilva', 'mariasilva@hotmail.com', '458', 'Av. Fast Fool', 'Sorocaba', 'SP', '45869-695', 'Jardim tempio', 'Sobrado', NULL),
+(45, 'familia', 0, 'OK', 'Selma House', '(15) 2565-2225', 'facebook.com.br/SelmaHouse', 'selmahouse@hotmail.com', '2536', 'Av. Fomseca', 'São Paulo', 'AC', '12598-696', 'Jardim Vitória', 'Ao lado da cisd', NULL),
+(46, 'doador_pf', 0, 'EA', 'JoÃ£o Santos', '(54) 5221-28888', 'facebook.com/josao_santos', 'joao@gmail.com', '252', 'Ruas da veradices', 'SÃ£o Paulo', 'SP', '55485-455', 'jardim cruzeiro', '', 'joao@gmail.com'),
+(48, 'doador_pf', 0, 'EA', 'JosÃ© Guilherme', '(54) 8148-88888', 'facebook.com/joseguilherme', 'jose.guilherme@hotmail.com', '1254', 'Rua Junior ', 'Mairinque', 'SP', '44825-236', 'Jardim Cruzeiro', 'Ao lado do Bom lugar', 'jose.guilherme@hotmail.com');
 
 -- --------------------------------------------------------
 
@@ -325,6 +382,13 @@ CREATE TABLE IF NOT EXISTS `perfil_exibe` (
   KEY `fk_id_mensagem` (`fk_id_mensagem`),
   KEY `fk_id_cadastro` (`fk_id_cadastro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `perfil_exibe`
+--
+
+INSERT INTO `perfil_exibe` (`fk_id_mensagem`, `fk_id_cadastro`) VALUES
+(5, 40);
 
 -- --------------------------------------------------------
 
@@ -362,7 +426,8 @@ CREATE TABLE IF NOT EXISTS `possui_cri` (
 
 INSERT INTO `possui_cri` (`fk_rg_crianca`, `fk_id_familia`) VALUES
 ('58.656.656-6', 1),
-('66.658.695-9', 2);
+('66.658.695-9', 2),
+('58.695.695-6', 3);
 
 -- --------------------------------------------------------
 
@@ -377,6 +442,16 @@ CREATE TABLE IF NOT EXISTS `realiza` (
   KEY `fk_id_doacao` (`fk_id_doacao`),
   KEY `fk_id_cadastro` (`fk_id_cadastro`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `realiza`
+--
+
+INSERT INTO `realiza` (`fk_id_doacao`, `fk_id_cadastro`) VALUES
+(52, 40),
+(53, 40),
+(54, 40),
+(55, 48);
 
 -- --------------------------------------------------------
 
@@ -397,6 +472,8 @@ CREATE TABLE IF NOT EXISTS `usuario` (
 
 INSERT INTO `usuario` (`user`, `senha`) VALUES
 ('doeparavida@doe.com', '123456'),
+('joao@gmail.com', '123456'),
+('jose.guilherme@hotmail.com', '15987532'),
 ('ose@gmail.com', '123456'),
 ('sorriso@sorriso.com.br', '123456');
 
