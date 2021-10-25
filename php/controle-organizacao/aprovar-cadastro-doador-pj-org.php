bd7cb9ca30db9104e4d8aef1b125fb428fb57d8e
joseguilherme96 <jose.guilherme96@outlook.com.br>
2021-10-25 00:42:42 -0300

    Organização de código

--- php/controle-organizacao/aprovar-cadastro-doador-pj-org.php
+++ php/controle-organizacao/aprovar-cadastro-doador-pj-org.php
@@ -4,18 +4,23 @@
  include '../controle-site/redirecionamento-pagina.php';
  include 'sessao-org.php';
  include '../controle-site/mensagens.php';
+ include '../controle-site/funcoes-sistema.php';
 
 if(isset($_GET['codigo'])){
  $perfil = 'SELECT * FROM perfil where id_cadastro="'.$_GET['codigo'].'"';
  $perfil = mysqli_query($conecta,$perfil);
-}else{
-   $perfil = 'SELECT * FROM perfil where id_cadastro="'.$_POST['codigo'].'"';
- $perfil = mysqli_query($conecta,$perfil);
-}
-
  foreach($perfil as $tipo_cadastro){
 
- }
+}
+}else if(isset($_POST['codigo'])){
+   $perfil = 'SELECT * FROM perfil where id_cadastro="'.$_POST['codigo'].'"';
+ $perfil = mysqli_query($conecta,$perfil);
+ foreach($perfil as $tipo_cadastro){
+
+}
+}
+
+ 
 
  if(isset($_GET['btnAprovar'])==1){
 
@@ -58,7 +63,7 @@ if(isset($_GET['codigo'])){
 
  sessao_mensagem(mensagem(29));
 
- }else if(isset($_POST['btnResolvido'])){
+ }else if(isset($_POST['btnResolvido'])){ 
     $id_cadastro_aprovacao = $_GET['codigo']; 
     $search = 'UPDATE mensagem_sistema SET status_sistema="FINALIZADO" WHERE id_mensagem="'.$_GET['id_mensagem'].'"';
     $result_search = mysqli_query($conecta, $search);
@@ -91,6 +96,59 @@ if(isset($_GET['codigo'])){
       sessao_mensagem(mensagem(30));
 
       }
- }
+ }else if(isset($_GET['btnAprovarDoacao'])){
+
+        $data=data_hora();
+        $search = 'UPDATE doacao SET status_doacao = "PENDENTE",data_hora_recebimento ="'.$data.'" WHERE  id_doacao = "'.$_GET['btnAprovarDoacao'].'"';
+        $result_search = mysqli_query($conecta, $search);
+        redireciona(16);
+        sessao_mensagem(mensagem(31));
+ }else if(isset($_POST['btnReprovarDoacao'])){
+    $id_doacao = $_POST['codigo'];
+    $search = "UPDATE doacao SET status_doacao = 'REPROVADO' WHERE id_doacao = '$id_doacao'";
+ $result_search = mysqli_query($conecta, $search);
+
+    $search = 'INSERT mensagem_sistema(status_sistema,mensagem) values("PENDENTE","'.$_POST['mensagem'].'")';
+ $result_search = mysqli_query($conecta, $search);
+
+ 
+ 
+ $id_mensagem = mysqli_insert_id($conecta);
+
+ $search = 'INSERT doa_exibe(fk_id_doacao,fk_id_mensagem) values("'.$_POST['codigo'].'","'.$id_mensagem .'")';
+ $result_search = mysqli_query($conecta, $search);
+ 
+
+    redireciona_get(19,$id_doacao); // Função retorna pagina com código da doação
+    sessao_mensagem(mensagem(32));
+
+ }else if(isset($_POST['btnResolvidoDoacao'])){
+  $id_doacao = $_POST['id_doacao']; 
+
+  echo $_POST['id_doacao']." ".$_POST['id_mensagem'];
+  $search = 'UPDATE mensagem_sistema SET status_sistema="FINALIZADO" WHERE id_mensagem="'.$_POST['id_mensagem'].'"';
+  $result_search = mysqli_query($conecta, $search);
+
+  $search = 'SELECT ms.status_sistema FROM mensagem_sistema ms
+                            INNER JOIN doa_exibe pe on pe.fk_id_mensagem=ms.id_mensagem WHERE ms.status_sistema="FINALIZADO" AND pe.fk_id_doacao="'.$id_doacao.'"';
+    $result_search = $conecta->query($search);
+
+    //Se não exitir mais mensagens referente ao cadastro do doador, o cadastro é aprovado.
+    if($conta=$result_search->num_rows==0){
+       $search = "UPDATE doacao SET status_doacao = 'PENDENTE' WHERE id_doacao = '$id_doacao'";
+       $result_search = mysqli_query($conecta, $search);
+
+       $data=data_hora();
+      $search = 'UPDATE doacao SET status_doacao = "PENDENTE",data_hora_recebimento ="'.$data.'" WHERE  id_doacao = "'.$id_doacao.'"';
+
+       
+      redireciona(16);
+      sessao_mensagem(mensagem(31));
+
+    }else{
+          redireciona_get(19,$id_doacao);
+          sessao_mensagem(mensagem(33));
+    }
+}
 
 ?>
